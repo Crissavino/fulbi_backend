19841c5d47824483c6c0cea773489949e82aaa68
cristiansavino <savinocristian89@gmail.com>
2021-06-26 10:15:16 +0100

    fix 1

--- database/migrations/2021_06_22_084134_add_image_to_users_table.php
+++ database/migrations/2021_06_22_084134_add_image_to_users_table.php
@@ -14,7 +14,7 @@ class AddImageToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_image')->default(null);
+            $table->string('profile_image')->nullable();
         });
     }
 
